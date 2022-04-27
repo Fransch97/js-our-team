7b6fa14578c0a7ea1fca238ab6bac86d6ad4b720
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-27 14:45:17 +0200

    bonus

--- script.js
+++ script.js
@@ -4,14 +4,8 @@ console.log("Hi master");
 // Viene fornito un layout di base in cui è presente **una card di esempio** inserita staticamente nell’html. Questa card serve solo come timbro del markup necessario per ottenere il risultato finale e **dovrà quindi essere rimossa dall’html**.
 // Aggiungere un file js in cui definire **un array di oggetti** che rappresentano i membri del team. Ogni membro ha le informazioni necessarie per stampare la relativa card: **Nome, Ruolo e Foto**.
 // Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell’html, stampare dinamicamente una card per ogni membro del team.
-// BONUS:
-// Utilizzare gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team: cliccando sul pulsante “add” viene creato un **nuovo oggetto**, il quale viene **inserito nell’array iniziale** e viene stampata una nuova card con tutte le informazioni inserite dall’utente.
-// **Consigli del giorno:**
-// Ragioniamo come sempre a step.
-// Scriviamo prima la logica in italiano e poi traduciamo in codice.
-// console.log() è nostro amico.
-// Quando un pezzo di codice funziona, chiediamoci se possiamo scomporlo in funzioni più piccole.
-// Buon lavoro! :muscolo:
+
+const randomNumber = () => Math.ceil(Math.random()*8)
 
 
 //creat team array object
@@ -79,3 +73,84 @@ for(let i in team){
     `;
 }
 
+// BONUS:
+// Utilizzare gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team: cliccando sul pulsante “add” viene creato un **nuovo oggetto**, il quale viene **inserito nell’array iniziale** e viene stampata una nuova card con tutte le informazioni inserite dall’utente.
+
+
+//get html element's
+// const gtElmnt = (element) => document.querySelector(element);
+// function gtElmnt(element){// LEARNED WHY DON'T DO THIS: don't feels for other methods from JS
+//     return  document.querySelector(element);  
+// }
+// const addinrHtml = (gtElmnt, msg) => gtElmnt.innerHTML += msg; //not needed for now just tested
+const gtValue = (element) => document.querySelector(element).value; // new idea
+
+//COMMENTED BECAUSE DON'T WORKS
+// console.log(gtElmnt('div'))//test
+// const nameInput = gtElmnt('#name');
+// console.log(nameInput)//test
+// const professionInput = gtElmnt('#role');
+// console.log(professionInput)//test
+// const imgInput = gtElmnt('#image');
+// console.log(imgInput)//test
+
+const btn = document.querySelector('#addMemberButton')//gtElmnt();
+console.log(btn)//test
+const inrTeamMmbr = (fullname, profession,imgID ) => {
+    team.push({
+        fullName: fullname,
+        profession: profession,
+        imgID: imgID
+    })
+}
+
+btn.addEventListener('click',()=>{
+    const valueName = gtValue('#name');
+    console.log(valueName)//tested
+    const valueProfession = gtValue('#role');
+    console.log(valueProfession)//tested
+    const valueimgID = gtValue('#image');
+    console.log(valueimgID)
+    inrTeamMmbr(valueName,valueProfession,valueimgID)
+    console.log(team)//tested
+
+    if(valueimgID === " "){
+
+        teamSectionHtml.innerHTML += 
+        `
+            <div class="team-card">
+                <div class="team-card">
+                    <div class="card-image">
+                    <img
+                        src="img/${valueimgID}"
+                        alt="${valueName}"
+                    />
+                    </div>
+                    <div class="card-text">
+                    <h3>${valueName}</h3>
+                    <p>${valueProfession}</p>
+                    </div>
+                </div>
+            </div>
+        `;
+    }else{
+        teamSectionHtml.innerHTML += 
+        `
+            <div class="team-card">
+                <div class="team-card">
+                    <div class="card-image">
+                    <img
+                        src="img/img2/no-img${randomNumber()}.jpg"
+                        alt="${valueName}"
+                    />
+                    </div>
+                    <div class="card-text">
+                    <h3>${valueName}</h3>
+                    <p>${valueProfession}</p>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+
+})
